0c0f721da751e6def461bb2e60204393894c7494
Leandro Guimarães <leguimas@gmail.com>
2012-12-13 17:21:30 -0200

    Create the environment specializations

--- src/main/java/br/com/dextra/expertus/environment/Environment.java
+++ src/main/java/br/com/dextra/expertus/environment/Environment.java
@@ -10,10 +10,19 @@ import org.openqa.selenium.WebDriver;
  */
 public abstract class Environment {
 
+	/**
+	 * The system property used to define the environment platform.
+	 */
 	protected static final String ENVIRONMENT_PLATFORM = "expertus.environment.platform";
 
+	/**
+	 * The system property used to define the browser that you want.
+	 */
 	protected static final String ENVIRONMENT_BROWSER = "expertus.environment.browser";
 
+	/**
+	 * The system property used to define the browser version that you want.
+	 */
 	protected static final String ENVIRONMENT_BROWSER_VERSION = "expertus.environment.browser.version";
 
 	private WebDriver driver;
